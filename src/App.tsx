e84fe8078aa4075e4e8df6c65db55c99d9e978c1
bartek9105 <bartek9105@gmail.com>
2022-05-14 15:46:37 +0200

    create graph for associated artists

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
 import { Input, Button, Flex, Container } from '@chakra-ui/react'
 import { useState } from 'react'
 import { useGetArtists } from './hooks/useGetArtists'
+import Graph from 'react-vis-network-graph'
+import { v4 as uuid } from 'uuid'
 
 function App() {
-	const [searchTerm, setSearchTerm] = useState('')
+	const [searchTerm, setSearchTerm] = useState('Michael Jackson')
 	const { artists, getArtists } = useGetArtists(searchTerm)
+
+	const edges = artists.map((artist, index) => {
+		return {
+			from: 1,
+			to: index + 1
+		}
+	})
+
+	console.log(edges)
+
+	const graph = {
+		nodes: artists,
+		edges: artists.map((_, index) => {
+			return {
+				from: 1,
+				to: index + 1
+			}
+		})
+	}
+	const options = {
+		layout: {
+			hierarchical: true
+		},
+		edges: {
+			color: '#000000'
+		},
+		height: '500px'
+	}
 
 	return (
 		<div className='App'>
@@ -19,12 +49,8 @@ function App() {
 						Search
 					</Button>
 				</Flex>
-				<ul>
-					{artists?.map((artist) => (
-						<li>{artist}</li>
-					))}
-				</ul>
 			</Container>
+			<Graph graph={graph} options={options} key={uuid()} />
 		</div>
 	)
 }
--- src/hooks/useGetArtists.ts
+++ src/hooks/useGetArtists.ts
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { getAssociatedArtists } from '../api/artists'
+import { v4 as uuid } from 'uuid'
 
 export const useGetArtists = (searchTerm: string) => {
 	const [artists, setArtists] = useState([])
@@ -8,7 +9,15 @@ export const useGetArtists = (searchTerm: string) => {
 		if (searchTerm) {
 			const getArtists: any = await getAssociatedArtists(searchTerm)
 			const data = getArtists.data.results.bindings
-			setArtists(data.map((el: any) => el.label.value))
+			setArtists(
+				data.map((el: any, index: number) => {
+					return {
+						label: el.label.value,
+						id: index + 1,
+						title: el.label.value
+					}
+				})
+			)
 		}
 	}
 
